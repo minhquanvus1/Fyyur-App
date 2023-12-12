15a3fb50721607d4270cc7541be40fc173e2f8b6
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-12-12 12:15:01 +0700

    add the local PostgreSQL database connection in the config.py, and create a Migrate object in app.py to generate migration file

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -21,7 +22,10 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# TODO: connect to a local postgresql database
+# TODO: connect to a local postgresql database (already done in config.py)
+
+# create a Migrate object to generate migration file
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
